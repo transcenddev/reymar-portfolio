29eeb758bb98d584b5419fcd0d21857b4e8ef3ed
dev <97653516+transcenddev@users.noreply.github.com>
2025-08-20 18:47:11 +0800

    fix(ui): ensure email + book a call buttons stack on mobile and align properly on desktop

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -68,8 +68,11 @@ const Footer: FC = () => {
               >
                 Enough talk. Let&apos;s make something great together.
               </h2>
-              <div className="flex gap-4">
-                <a href="mailto:2049rare@gmail.com">
+              <div className="flex flex-col lg:flex-row gap-4 ">
+                <a
+                  href="mailto:2049rare@gmail.com"
+                  className="w-full lg:w-auto"
+                >
                   <Button
                     variant="secondary"
                     className="mt-8"
@@ -111,49 +114,54 @@ const Footer: FC = () => {
                     2049rare@gmail.com
                   </Button>
                 </a>
-                <Button
-                  variant="secondary"
-                  className="mt-8"
-                  onClick={() =>
-                    window.open("https://calendly.com/2049rare/30min", "_blank")
-                  }
-                  iconAfter={
-                    <div className="size-6 overflow-hidden">
-                      <div className="w-12 h-6 flex transition-all duration-300 group-hover/button:-translate-x-1/2">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="size-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
-                          />
-                        </svg>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="size-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
-                          />
-                        </svg>
+                <div className="w-full lg:w-auto">
+                  <Button
+                    variant="secondary"
+                    className="mt-4"
+                    onClick={() =>
+                      window.open(
+                        "https://calendly.com/2049rare/30min",
+                        "_blank"
+                      )
+                    }
+                    iconAfter={
+                      <div className="size-6 overflow-hidden">
+                        <div className="w-12 h-6 flex transition-all duration-300 group-hover/button:-translate-x-1/2">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="size-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
+                            />
+                          </svg>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="size-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
+                            />
+                          </svg>
+                        </div>
                       </div>
-                    </div>
-                  }
-                >
-                  Book a call
-                </Button>
+                    }
+                  >
+                    Book a call
+                  </Button>
+                </div>
               </div>
             </div>
             <div>
