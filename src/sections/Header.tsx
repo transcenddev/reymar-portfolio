95fe2144a6f89763efdb4ecedc5daaa34f0a4697
dev <97653516+transcenddev@users.noreply.github.com>
2025-08-20 18:16:18 +0800

    fix: replace 'class' with 'className' in Header.tsx SVG

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -164,11 +164,11 @@ const Header: FC = () => {
                   viewBox="0 0 24 24"
                   strokeWidth="1.5"
                   stroke="currentColor"
-                  class="size-6"
+                  className="size-6"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
                   />
                 </svg>
